5e4459875f96a9a0f17f5b24fa080c1ec52aec3a
Alexis Martin <alexismartin1895@gmail.com>
2023-10-06 16:27:31 -0700

    setting up private data

--- database.js
+++ database.js
@@ -1,7 +1,19 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
 
 function connectToDatabase() {
+	const serviceAccount = {
+		type: "service_account",
+		project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+		private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+		private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+		client_email: process.env.CLIENT_EMAIL,
+		client_id: process.env.CLIENT_ID,
+		auth_uri: process.env.AUTH_URI,
+		token_uri: process.env.TOKEN_URI,
+		auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+		client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+		universe_domain: process.env.UNIVERSE_DOMAIN,
+	};
 	admin.initializeApp({
 		credential: admin.credential.cert(serviceAccount),
 	});
